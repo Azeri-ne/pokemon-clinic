ede130c6171f90cc23b56fe1b389342e13bb222d
Azeri-ne <loudenhal@protonmail.com>
2026-08-01 11:29:45 +0800

    Add placeholder variables for Owner and Nurse

--- pokemon-clinic/clinic/models.py
+++ pokemon-clinic/clinic/models.py
@@ -4,6 +4,7 @@ class OwnerModel():
     name = models.CharField(max_length=50)
     email = models.CharField(max_length=50)
     password = models.CharField()
+    #owned_pokemon
 
     def __str__(self):
         return self.name
@@ -12,6 +13,7 @@ class NurseModel():
     name = models.CharField(max_length=50)
     email = models.CharField(max_length=50)
     password = models.CharField()
+    #managed_pokemon
 
     def __str__(self):
         return self.name
