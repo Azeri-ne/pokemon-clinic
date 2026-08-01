31e097c51b4abade94694f865d74d7af75e0b710
Azeri-ne <loudenhal@protonmail.com>
2026-08-01 11:29:12 +0800

    Add NurseModel

--- pokemon-clinic/clinic/models.py
+++ pokemon-clinic/clinic/models.py
@@ -7,3 +7,11 @@ class OwnerModel():
 
     def __str__(self):
         return self.name
+
+class NurseModel():
+    name = models.CharField(max_length=50)
+    email = models.CharField(max_length=50)
+    password = models.CharField()
+
+    def __str__(self):
+        return self.name
