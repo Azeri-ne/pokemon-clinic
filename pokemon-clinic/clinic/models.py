59f180701a12db0523c48b8bdd59b5dce7553b9d
Azeri-ne <loudenhal@protonmail.com>
2026-08-01 11:26:45 +0800

    Create OwnerModel

--- pokemon-clinic/clinic/models.py
+++ pokemon-clinic/clinic/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class OwnerModel():
+    name = models.CharField(max_length=50)
+    email = models.CharField(max_length=50)
+    password = models.CharField()
+
+    def __str__(self):
+        return self.name
